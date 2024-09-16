f877dc4040bc748189b2c635d894a853f5901035
Apinya <159605386+PrangApinya@users.noreply.github.com>
2024-09-16 22:28:07 +0700

    ีup

--- frontend/src/components/cafehead/Cafe.jsx
+++ frontend/src/components/cafehead/Cafe.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Cafe.css'
-import logo from '../../assets/img/logo.png'
 import coffeecup from '../../assets/img/coffeecup.png'
 import icedcoffee from '../../assets/img/icedcoffee.png'
 import cake from '../../assets/img/cake.png'
 import harvest from '../../assets/img/harvest.png'
 
+const openNav = () => {
+  document.getElementById("myNav").style.display = "block";
+}
+
+const closeNav = () => {
+  document.getElementById("myNav").style.display = "none";
+}
 const Cafe = () => {
  
  
@@ -14,6 +20,20 @@ const Cafe = () => {
     <header>
      
       <div class="navigation">
+<div class="burger">
+  <div id="myNav" className="overlay">
+    <a href="javascript:void(0)" className="closebtn" onClick={closeNav}>×</a>
+    <div className="overlay-content">
+      <a href="#">DASHBOARD</a>
+      <a href="#">STAFF</a>
+     
+    </div>
+  </div>
+  <span style={{fontSize: 30, cursor: 'pointer'}} onClick={openNav}>☰ </span>
+    </div>
+
+
+    
        
         <div class="image">
   
