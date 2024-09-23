c920ec333980fd8653e90bc83c58ab7c80ec31df
Apinya <159605386+PrangApinya@users.noreply.github.com>
2024-09-23 23:53:05 +0700

    up

--- frontend/src/components/page/Pagehot.jsx
+++ frontend/src/components/page/Pagehot.jsx
@@ -7,8 +7,8 @@ import tea from '../../assets/img/tea.png'
 import teat from '../../assets/img/teat.png'
 
 const Pagehot = () => {
-    
-    
+   
+
     return (
         <div>
             <Cafe />
@@ -21,11 +21,10 @@ const Pagehot = () => {
                                 <div className="card-body">
                                     <h4>latte</h4>
                                     <h5>40THB</h5>
-                                    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                    Add menu </button>
+                                    <button class="btn btn-primary" onclick="window.location.href='#';"><h5>Add menu</h5></button>
                                 </div>
                             </div>
-                        </div> 
+                        </div>
 
                         <div className="col-3">
                             <div className="card ms-5">
@@ -33,8 +32,7 @@ const Pagehot = () => {
                                 <div className="card-body">
                                     <h4>Cappuccino</h4>
                                     <h5>45THB</h5>
-                                    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                    Add menu </button>
+                                    <button class="btn btn-primary" onclick="window.location.href='#';"><h5>Add menu</h5></button>
                                 </div>
                             </div>
                         </div>
@@ -45,8 +43,7 @@ const Pagehot = () => {
                                 <div className="card-body">
                                     <h4>Green tea</h4>
                                     <h5>30THB</h5>
-                                    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                    Add menu </button>
+                                    <button class="btn btn-primary" onclick="window.location.href='#';"><h5>Add menu</h5></button>
                                 </div>
                             </div>
                         </div>
@@ -57,50 +54,13 @@ const Pagehot = () => {
                                 <div className="card-body">
                                     <h4>thai tea</h4>
                                     <h5>30THB</h5>
-                                    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                    Add menu </button>
+                                    <button class="btn btn-primary" onclick="window.location.href='#';"><h5>Add menu</h5></button>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                
-                <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div className="modal-dialog">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            
-                            <div class="dropdown">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            sweetness
-                            </button>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Less sweet</a></li>
-                                <li><a class="dropdown-item" href="#">Normal sweet</a></li>
-                                <li><a class="dropdown-item" href="#">Very sweet</a></li>
-                            </ul>
-                            </div>
-                            <div class="dropdown">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            topping
-                            </button>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">whipped cream</a></li>
-                                
-                            </ul>
-                            </div>
 
-                    
-                        
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-primary">Add menu</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     )
