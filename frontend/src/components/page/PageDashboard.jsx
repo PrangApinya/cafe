92c5bd4c850c0c577de8cb3c7d84be9127647ede
Donny <tanadornrach@gmail.com>
2024-12-06 21:45:21 +0700

    update dashboard

--- frontend/src/components/page/PageDashboard.jsx
+++ frontend/src/components/page/PageDashboard.jsx
@@ -18,9 +18,10 @@ const PageDashboard = () => {
         try {
             const response = await axios.get('http://localhost:8085/receipts/top-menu-sales');
             const formattedData = response.data.map(item => ({
-                ...item,
+                totalQuantity: item.total_quantity,
                 combinedLabel: `${item.menu.name} (${item.menu.type})`,
             }));
+            console.log(formattedData);
             setTopMenuSales(formattedData);
         } catch (error) {
             console.error('Error fetching top menu sales:', error);
@@ -34,9 +35,11 @@ const PageDashboard = () => {
             const formattedData = response.data.map(item => ({
                 menuName: item.menu.name,
                 menuType: item.menu.type,
-                total_quantity: item.total_quantity,
-                total_price: item.total_price
+                combinedLabel: `${item.menu.name} (${item.menu.type})`,
+                totalQuantity: item.total_quantity,
+                totalPrice: item.total_price
             }));
+            console.log(formattedData);
             setDailySales(formattedData);
         } catch (error) {
             console.error('Error fetching daily sales:', error);
@@ -48,9 +51,13 @@ const PageDashboard = () => {
         try {
             const response = await axios.get('http://localhost:8085/receipts/monthly-sales');
             const formattedData = response.data.map(item => ({
-                ...item,
-                sale_month: new Date(item.sale_month).toLocaleString('default', { month: 'long', year: 'numeric' }),
+                menuName: item.menu.name,
+                menuType: item.menu.type,
+                combinedLabel: `${item.menu.name} (${item.menu.type})`,
+                totalQuantity: item.total_quantity,
+                totalPrice: item.total_price
             }));
+            console.log(formattedData);
             setMonthlySales(formattedData);
         } catch (error) {
             console.error('Error fetching monthly sales:', error);
@@ -63,9 +70,9 @@ const PageDashboard = () => {
         fetchMonthlySales();
     }, []);
 
-    // Custom label for daily sales to display menu name, type, quantity, and total price
-    const renderDailySalesLabel = ({ menuName, menuType, total_quantity, total_price }) => {
-        return `${menuName} (${menuType}) : ${total_quantity} items ($${total_price.toFixed(2)})`;
+    // Custom label for sales to display menu name, type, quantity, and total price
+    const renderSalesLabel = ({ menuName, menuType, totalQuantity, totalPrice }) => {
+        return `${menuName} (${menuType}) : ${totalQuantity} items (${totalPrice.toFixed(2)} THB)`;
     };
 
     return (
@@ -76,60 +83,64 @@ const PageDashboard = () => {
             <h1>Dashboard</h1>
 
             {/* Top Menu Sales Bar Chart */}
-            <h2>Top Menu Sales</h2>
-            <div className="chart-container">
+            <div className="bar-container">
+                <h2>Top Menu Sales</h2>
                 <BarChart width={1000} height={300} data={topMenuSales}>
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis dataKey="combinedLabel" />
                     <YAxis />
                     <Tooltip />
                     <Legend />
-                    <Bar dataKey="total_quantity" fill="#8884d8" />
+                    <Bar dataKey="totalQuantity" fill="#8884d8" />
                 </BarChart>
             </div>
 
             {/* Daily Sales Pie Chart */}
-            <h2>Daily Sales</h2>
-            <div className="chart-container">
-                <PieChart width={800} height={300}>
-                    <Pie
-                        data={dailySales}
-                        dataKey="total_quantity"
-                        nameKey="menu.name"
-                        cx="50%"
-                        cy="50%"
-                        outerRadius={100}
-                        fill="#8884d8"
-                        label={renderDailySalesLabel}
-                    >
-                        {dailySales.map((entry, index) => (
-                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                        ))}
-                    </Pie>
-                    <Tooltip />
-                </PieChart>
+            <div className="pie-container">
+                <h2>Daily Sales</h2>
+                { dailySales.length === 0 ? <h3 className="no-sale">No sale for today yet.</h3> :
+                    <PieChart width={1000} height={300}>
+                        <Pie
+                            data={dailySales}
+                            dataKey="totalQuantity"
+                            nameKey="combinedLabel"
+                            cx="50%"
+                            cy="50%"
+                            outerRadius={100}
+                            fill="#8884d8"
+                            label={renderSalesLabel}
+                        >
+                            {dailySales.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                            ))}
+                        </Pie>
+                        <Tooltip />
+                    </PieChart>
+                }
             </div>
 
             {/* Monthly Sales Pie Chart */}
-            <h2>Monthly Sales</h2>
-            <div className="chart-container">
-                <PieChart width={600} height={300}>
-                    <Pie
-                        data={monthlySales}
-                        dataKey="total_quantity"
-                        nameKey="sale_month"
-                        cx="50%"
-                        cy="50%"
-                        outerRadius={100}
-                        fill="#82ca9d"
-                        label
-                    >
-                        {monthlySales.map((entry, index) => (
-                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                        ))}
-                    </Pie>
-                    <Tooltip />
-                </PieChart>
+            <div className="pie-container">
+                <h2>Monthly Sales</h2>
+                { monthlySales.length === 0 ? <h3 className="no-sale">No sale for this month yet.</h3> :
+                    <PieChart width={1000} height={300}>
+                        <Pie
+                            data={monthlySales}
+                            dataKey="totalQuantity"
+                            nameKey="combinedLabel"
+                            cx="50%"
+                            cy="50%"
+                            outerRadius={100}
+                            fill="#82ca9d"
+                            label={renderSalesLabel}
+                        >
+                            {monthlySales.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                            ))}
+                        </Pie>
+                        <Tooltip />
+                    </PieChart>
+                }
             </div>
         </div>
     );
